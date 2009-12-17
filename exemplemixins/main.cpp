8816511b06587250b6e5af2ee48e2dff7069baac
Carles F. Julià <carles.fernandez@upf.edu>
2009-12-17 22:10:24 +0000

    exemple de mixins
    git-svn-id: svn+ssh://fernandezcnau.ca.upf.edu/var/svn/tangibleframework/trunk/TSIframework@16 7fc800ec-ad5f-4a19-ad97-e970a811820c

---
+++ exemplemixins/main.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+using namespace std;
+
+// Define a new mixin class with a constructor that accepts all arguments.
+
+//inspirat per http://www.ddj.com/cpp/184404445
+
+class GenericCallback
+{
+    public:
+    virtual void run()=0;
+};
+
+template <class T>
+class Callback: public GenericCallback
+{
+    public:
+    Callback(T * i)
+    {
+        instance = i;
+    }
+    T * instance ;
+    void run()
+    {
+        //dynamic_cast<T>()
+        instance->runme();
+    }
+};
+
+class Customer
+{
+public:
+    Customer()
+    {
+
+    }
+    void print()
+    {
+        list<GenericCallback*>::iterator it;
+        for (it = capacitats.begin(); it != capacitats.end(); it++)
+        {
+            (*it)->run();
+        }
+    }
+    list<GenericCallback*> & getCapacitats()
+    {
+        return capacitats;
+    }
+    list<GenericCallback*> capacitats;
+};
+
+template <class Base>
+class Sapnedar : public Base
+{
+public:
+    Sapnedar()
+    {
+        cout << "aprenent a nedar" << endl;
+        Base::getCapacitats().push_back(new Callback<Sapnedar>(this));
+    }
+    void runme()
+    {
+        neda();
+    }
+    void neda()
+    {
+        cout << "Nedo" << endl;
+    }
+    list<GenericCallback*> & getCapacitats()
+    {
+        return Base::getCapacitats();
+    }
+};
+
+template <class Base>
+class SapCaminar : public Base
+{
+public:
+    SapCaminar()
+    {
+        cout << "aprenent a caminar" << endl;
+        Base::getCapacitats().push_back(new Callback<SapCaminar>(this));
+    }
+    void runme()
+    {
+        camina();
+    }
+    virtual void camina()
+    {
+        cout << "Camino" << endl;
+    }
+    list<GenericCallback*> & getCapacitats()
+    {
+        return Base::getCapacitats();
+    }
+};
+//template <class Base>
+class Especialitzacio: public SapCaminar<Sapnedar<Customer> >
+{
+    public:
+    void camina()
+    {
+        cout << "M'arrossego" << endl;
+    }
+};
+
+int main()
+{
+//SapCaminar<Sapnedar<Customer> > c;
+//    Sapnedar c;
+Especialitzacio c;
+    c.print();
+    c.neda();
+    return 0;
+}
+
+
