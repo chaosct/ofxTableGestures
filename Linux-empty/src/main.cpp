c92249f2ef3648aae89aea9aabc5edf894de2fd5
Carles F. Julià <carles.fernandez@upf.edu>
2010-01-07 20:33:52 +0000

    Linux template acabat
    git-svn-id: svn+ssh://fernandezcnau.ca.upf.edu/var/svn/tangibleframework/trunk/TSIframework@41 7fc800ec-ad5f-4a19-ad97-e970a811820c

--- Linux-empty/src/main.cpp
+++ Linux-empty/src/main.cpp
@@ -1,21 +1,14 @@
 #include "ofMain.h"
 #include "testApp.h"
 #include "ofAppGlutWindow.h"
-#include "tuioinput.h"
+
 
 //========================================================================
 int main( ){
 
     ofAppGlutWindow window;
 	ofSetupOpenGL(&window, 1024 ,768, OF_WINDOW);			// <-------- setup the GL context
-    //ofSetFrameRate(30);
-    //ofSetVerticalSync(true);
 
-    testApp * ta = new testApp();
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(ta);
+    ofRunApp(new testApp());
 
 }
--- Linux-empty/src/testApp.h
+++ Linux-empty/src/testApp.h
@@ -9,27 +9,16 @@
 
 using namespace tuio;
 
-class testApp : public CanDirectObjects< CanDirectFingers<tuioApp <TableApp> > >
+class testApp : public tuioApp <TableApp>
 {
 private:
 
-    double dx, dy;
-    DirectFinger * mydf;
-    DirectObject* mdo;
 
 public:
 
     void Setup();
     void Update();
     void Draw();
-
-    void updateTuioCursor(int32 id, float xpos,float ypos,float xspeed,float yspeed,float maccel);
-
-    void newCursor(int32 id, DirectFinger * df);
-    void removeCursor(int32 id);
-
-    void newObject(int32 s_id, int32 f_id, DirectObject *);
-    void removeObject(int32 s_id, int32 f_id);
 
     void WindowResized(int w, int h);
 
