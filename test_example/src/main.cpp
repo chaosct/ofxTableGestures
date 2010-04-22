ee9f7a824dac5fbc6edd459ae27901e07a4c9888
Carles F. Julià <carles.fernandez@upf.edu>
2010-04-22 19:59:04 +0200

    Fullscreen on XML

--- test_example/src/main.cpp
+++ test_example/src/main.cpp
@@ -1,14 +1,15 @@
 #include "ofMain.h"
 #include "testApp.h"
 #include "ofAppGlutWindow.h"
+#include "GlobalConfig.hpp"
 
-//========================================================================
+
 int main( ){
 
     ofAppGlutWindow window;
-    ofSetupOpenGL(&window, 1024 ,768, OF_WINDOW);			// <-------- setup the GL context
-	//for having 100% full screen you must change the previous
-	//declaration for this one:
-	//ofSetupOpenGL(&window, 1024 ,768, OF_GAME_MODE);
+    if(GlobalConfig::getRef("PROGRAM:FULLSCREEN",0))
+        ofSetupOpenGL(&window, 1024 ,768, OF_GAME_MODE);
+    else
+        ofSetupOpenGL(&window, 1024 ,768, OF_WINDOW);
 	ofRunApp( new testApp());
 }
