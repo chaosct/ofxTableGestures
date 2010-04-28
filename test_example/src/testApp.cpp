e013787534a7266c900de0a079dba55fb2f422da
Carles F. Julià <carles.fernandez@upf.edu>
2010-04-28 12:24:39 +0200

    Clean testApp.cpp

--- test_example/src/testApp.cpp
+++ test_example/src/testApp.cpp
@@ -1,31 +1,14 @@
 #include "testApp.h"
 #include "CursorFeedback.hpp"
 #include "FigureFeedback.hpp"
-#include "AreaGesture.hpp"
 
-class AreaProva: public Area
-{
-    public:
-    bool isInside(float x, float y)
-    {
-        return (x > y);
-    }
-};
 
-class AreaProva2: public Area
-{
-    public:
-    bool isInside(float x, float y)
-    {
-        return (x > 0.5f);
-    }
-};
+
 
 //--------------------------------------------------------------
 void testApp::Setup(){
-    new CursorFeedback(new AreaProva());
-    new CursorFeedback(new AreaProva2());
-    new FigureFeedback(RoundTableArea::Create());
+    new CursorFeedback();
+    new FigureFeedback();
 }
 
 //--------------------------------------------------------------
