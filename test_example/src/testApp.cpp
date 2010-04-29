a0508bd35b29cb0301d49d289d64906f98a7f336
Carles F. Julià <carles.fernandez@upf.edu>
2010-04-29 12:18:10 +0200

    Added InputGestureTap and feedback

--- test_example/src/testApp.cpp
+++ test_example/src/testApp.cpp
@@ -1,14 +1,13 @@
 #include "testApp.h"
 #include "CursorFeedback.hpp"
 #include "FigureFeedback.hpp"
-
-
-
+#include "TapFeedback.hpp"
 
 //--------------------------------------------------------------
 void testApp::Setup(){
     new CursorFeedback();
     new FigureFeedback();
+    new TapFeedback();
 }
 
 //--------------------------------------------------------------
