3d938eb225943715798e18dd416d4e4303826c21
Carles F. Julià <carles.fernandez@upf.edu>
2010-06-07 13:42:30 +0200

    adding allObjects and allFingers controllers.

---
+++ ofxTableGestures/src/Controllers/allFingers.hpp
@@ -0,0 +1,20 @@
+#ifndef ALLFINGERS_HPP_INCLUDED
+#define ALLFINGERS_HPP_INCLUDED
+
+#include "Graphic.hpp"
+#include "InputGestureDirectFingers.hpp"
+class allFingers: public OnTable < tuio::CanDirectFingers < Graphic > >
+{
+    public:
+    std::set<tuio::DirectFinger *> fingers;
+    void newCursor(tuio::DirectFinger * object)
+    {
+        fingers.insert(object);
+    }
+    void removeCursor(tuio::DirectFinger * object)
+    {
+        fingers.erase(object);
+    }
+};
+
+#endif // ALLFINGERS_HPP_INCLUDED
---
+++ ofxTableGestures/src/Controllers/allObjects.hpp
@@ -0,0 +1,21 @@
+#ifndef ALLOBJECTS_HPP_INCLUDED
+#define ALLOBJECTS_HPP_INCLUDED
+
+#include "Graphic.hpp"
+#include "InputGestureDirectObjects.hpp"
+
+class allObjects: public OnTable < tuio::CanDirectObjects < Graphic > >
+{
+    public:
+    std::set<tuio::DirectObject *> objects;
+    void newObject(tuio::DirectObject * object)
+    {
+        objects.insert(object);
+    }
+    void removeObject(tuio::DirectObject * object)
+    {
+        objects.erase(object);
+    }
+};
+
+#endif // ALLOBJECTS_HPP_INCLUDED
